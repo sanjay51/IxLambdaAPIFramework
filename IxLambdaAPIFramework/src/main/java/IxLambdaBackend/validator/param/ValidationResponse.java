dad7094f00c05000174735709cebe3f2dee35ed7
Sanjay Verma <sanjav@amazon.com>
2019-07-06 17:18:44 -0700

    Add simple constructor for ValidationResponse

--- IxLambdaAPIFramework/src/main/java/IxLambdaBackend/validator/param/ValidationResponse.java
+++ IxLambdaAPIFramework/src/main/java/IxLambdaBackend/validator/param/ValidationResponse.java
@@ -1,9 +1,16 @@
 package IxLambdaBackend.validator.param;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ValidationResponse {
     final boolean isValid;
     final String message;
+
+    public ValidationResponse(final boolean isValid) {
+        this.isValid = isValid;
+        this.message = "";
+    }
 }
