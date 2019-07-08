2517232a7b0a45da1ccaabcf44834fd9267d51f2
Sanjay Verma <sanjav@amazon.com>
2019-07-07 19:41:08 -0700

    API: Add Access-Control-Allow-Origin (+1 more) header to response

--- IxLambdaAPIFramework/src/main/java/IxLambdaBackend/response/Response.java
+++ IxLambdaAPIFramework/src/main/java/IxLambdaBackend/response/Response.java
@@ -4,14 +4,20 @@ import com.google.gson.Gson;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 public class Response {
     static final Gson gson = new Gson(); // TODO note: By default Gson would discard null values, change that.
+    private Map<String, String> headers = new HashMap<String, String>() {{
+        put("Access-Control-Allow-Origin", "*");
+        put("Access-Control-Allow-Headers", "Content-Type");
+    }};
 
     boolean isBase64Encoded;
     int statusCode;
-    Object headers;
     Object multiValueHeaders;
     Object body;
 
