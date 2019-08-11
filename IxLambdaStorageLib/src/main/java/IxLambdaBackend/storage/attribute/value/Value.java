e41cb43fe8cd21c155c0269170d7bb93f8657268
Sanjay Verma <sanjayvermaseattle@gmail.com>
2019-08-11 16:20:56 -0700

    Handle null values in Value

--- IxLambdaStorageLib/src/main/java/IxLambdaBackend/storage/attribute/value/Value.java
+++ IxLambdaStorageLib/src/main/java/IxLambdaBackend/storage/attribute/value/Value.java
@@ -3,7 +3,7 @@ package IxLambdaBackend.storage.attribute.value;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public abstract class Value<T> {
     //https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBMapper.DataTypes.html
@@ -16,16 +16,20 @@ public abstract class Value<T> {
 
         switch (type) {
             case NUMBER:
+                if (attributeValue.getN() == null) return new NullValue();
                 return new NumberValue(attributeValue.getN());
 
             case BOOLEAN:
+                if (attributeValue.getB() == null) return new NullValue();
                 return new BooleanValue(attributeValue.getBOOL());
 
             case STRING:
+                if (attributeValue.getS() == null) return new NullValue();
                 return new StringValue(attributeValue.getS());
 
             case STRING_SET:
-                return new StringSetValue(new HashSet<>(attributeValue.getSS()));
+                List<String> ss = attributeValue.getSS();
+                return new StringSetValue(ss == null? new HashSet<>(): new HashSet<>(ss));
 
             default:
                 return new NullValue();
