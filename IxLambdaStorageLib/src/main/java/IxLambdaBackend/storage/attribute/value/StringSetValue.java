7cf463a1fca66025bff26fc7d57bcd6132f54ae1
Sanjay Verma <sanjayvermaseattle@gmail.com>
2019-08-18 13:00:32 -0700

    Fix StringSetValue empty bug

--- IxLambdaStorageLib/src/main/java/IxLambdaBackend/storage/attribute/value/StringSetValue.java
+++ IxLambdaStorageLib/src/main/java/IxLambdaBackend/storage/attribute/value/StringSetValue.java
@@ -1,6 +1,7 @@
 package IxLambdaBackend.storage.attribute.value;
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.util.CollectionUtils;
 import lombok.AllArgsConstructor;
 
 import java.util.Set;
@@ -11,7 +12,10 @@ public class StringSetValue extends Value<Set<String>> {
 
     @Override
     public AttributeValue toDynamoDBAttributeValue() {
-        return new AttributeValue().withSS();
+        if (CollectionUtils.isNullOrEmpty(value))
+            return new AttributeValue().withSS();
+        
+        return new AttributeValue().withSS(value);
     }
 
     @Override
